e78af54af329f0c54aef795aa3b577e95fbef04f
Ihsan Fadlurrahman <ihsanfdrn@gmail.com>
2026-02-17 23:36:13 +0700

    Refactor logout link in sidebar: simplify logout functionality by removing JavaScript event handling and hiding the logout form

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -73,16 +73,15 @@
 
     {{-- Logout --}}
     <div class="sidebar-footer">
-        <a href="{{ route('logout') }}"
-           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+        <a href="{{ route('logout') }}">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
             </svg>
             Logout
         </a>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display:none;">
+        {{-- <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display:none;">
             @csrf
-        </form>
+        </form> --}}
     </div>
 
 </div>
